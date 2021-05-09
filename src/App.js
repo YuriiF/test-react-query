87a67c0eda77437f4f984490b6ce6becf358781c
Yurii Fotul <fotul.yurii@gmail.com>
2021-05-09 08:42:10 +0200

    Update package.json

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import Projects from './Projects';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Projects />
     </div>
   );
-}
+};
 
 export default App;
